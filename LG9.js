c83551e8be1aed6a1908af65d112607bc809886a
Mhycka <95332273+mhyckaherviananda12@users.noreply.github.com>
2025-07-19 11:02:44 +0700

    LG9.js

--- LG9.js
+++ LG9.js
@@ -8,9 +8,9 @@ function bukaPenilaian(tipe) {
     "1.6": "https://drive.google.com/file/d/1YyIKNijALaBPjDs0grrlI-PNTz2YXmBe/view?usp=sharing",
     "1.7": "https://drive.google.com/file/d/1WgjtWYI8UZm-RBtGSL1ZQrCjv8Y7uJq_/view?usp=sharing",
     "1.8": "https://drive.google.com/drive/folders/1_9VfW6I0VI-IgVfmE1zxUhgM7IoSaVtw?usp=sharing",
-    "1.9": "./buku-penerimaan-rapor.html",
+    "1.9": "buku-penerimaan-rapor.html",
     "1.10": "https://drive.google.com/file/d/1kM9n_fPsBn2x0efWv-r8V9sBwiU5RCRQ/view?usp=sharing",
-    "1.11": "./Remedial-dan-Pengayaan.html",
+    "1.11": "Remedial-dan-Pengayaan.html",
     "1.12": "https://drive.google.com/file/d/1XVFRrlha9agVKXOn3mk1UOYaSUxDeOob/view?usp=sharing",
   };
 
